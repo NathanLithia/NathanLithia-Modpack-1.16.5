f75bcd12de602cc3d6450425bcb69f07486af0af
Nathan - Lithia <nathanlol5@live.com.au>
2021-07-12 13:31:56 +0930

    Create Create2IEPlates.js
    Method of creating IE plates with Create Press

---
+++ kubejs/server_scripts/Create2IEPlates.js
@@ -0,0 +1,12 @@
+//IE Plate Recipes for Create Press
+// Output, Input
+onEvent('recipes', event => {
+    event.recipes.create.pressing('immersiveengineering:plate_aluminum', '#forge:ingots/aluminum')
+    event.recipes.create.pressing('immersiveengineering:plate_lead', '#forge:ingots/lead')
+    event.recipes.create.pressing('immersiveengineering:plate_silver', '#forge:ingots/silver')
+    event.recipes.create.pressing('immersiveengineering:plate_nickel', '#forge:ingots/nickel')
+    event.recipes.create.pressing('immersiveengineering:plate_uranium', '#forge:ingots/uranium')
+    event.recipes.create.pressing('immersiveengineering:plate_constantan', '#forge:ingots/constantan')
+    event.recipes.create.pressing('immersiveengineering:plate_electrum', '#forge:ingots/electrum')
+    event.recipes.create.pressing('immersiveengineering:plate_steel', '#forge:ingots/steel')
+})
