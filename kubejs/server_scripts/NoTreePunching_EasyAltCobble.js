50d4edc89ded84669feef1f4cfe98bd793c09c73
Nathan - Lithia <nathanlol5@live.com.au>
2021-08-24 03:14:12 +0930

    Create NoTreePunching_EasyAltCobble.js

---
+++ kubejs/server_scripts/NoTreePunching_EasyAltCobble.js
@@ -0,0 +1,99 @@
+onEvent('recipes', event => {
+    event.shaped('3x minecraft:cobblestone', [
+      'SS ',
+      'S  ',
+      '   '
+    ], {
+      S: 'minecraft:stone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('3x notreepunching:granite_cobblestone', [
+      'SS ',
+      'S  ',
+      '   '
+    ], {
+      S: 'minecraft:granite',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('3x notreepunching:diorite_cobblestone', [
+      'SS ',
+      'S  ',
+      '   '
+    ], {
+      S: 'minecraft:diorite',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('3x notreepunching:andesite_cobblestone', [
+      'SS ',
+      'S  ',
+      '   '
+    ], {
+      S: 'minecraft:andesite',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:granite_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'notreepunching:granite_cobblestone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:stone_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'minecraft:cobblestone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:andesite_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'notreepunching:andesite_cobblestone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:diorite_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'notreepunching:diorite_cobblestone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:sandstone_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'minecraft:sandstone',
+    })
+})
+
+onEvent('recipes', event => {
+    event.shaped('4x notreepunching:red_sandstone_loose_rock', [
+      'S  ',
+      '   ',
+      '   '
+    ], {
+      S: 'minecraft:red_sandstone',
+    })
+})
