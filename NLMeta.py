0c6de98c703a8c9b6e7c80806e14c7fbcb62beb2
Nathan - Lithia <nathanlol5@live.com.au>
2021-09-22 21:43:45 +0930

    Update NLMeta.py

--- NLMeta.py
+++ NLMeta.py
@@ -8,21 +8,12 @@ BLOCK_SIZE = 65536
 
 for mod in mods:
     if mod.endswith((".NLMeta")):
-        print(f"Updated: {mod}")
         os.remove("mods/"+mod)
 for mod in mods:
     if mod.endswith((".jar")):
-        print(f"Wrote: {mod}")
-
         modhash = hashlib.md5(open(f'mods/'+mod,'rb').read()).hexdigest()
-
-        #with open("mods/"+mod, 'rb') as f:
-        #    filebytes = f.read(BLOCK_SIZE)
-        #    while len(filebytes) > 0:
-        #        md5.update(filebytes)
-        #        filebytes = f.read(BLOCK_SIZE)
-
         f = open(f"mods/{mod}.NLMeta", "a")
         f.write(f"{mod}\n{modhash}")
         f.close()
+        print(f"{modhash} {mod}")
 
