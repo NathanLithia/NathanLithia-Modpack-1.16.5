12cc46fe958ac5559f1c0b05352c88aea82f60d0
Nathan - Lithia <nathanlol5@live.com.au>
2021-11-30 17:05:28 +1030

    Stalwart Client crash bug

--- kubejs/server_scripts/blacklist.js
+++ kubejs/server_scripts/blacklist.js
@@ -7,9 +7,9 @@ function relaxedContains (thing,array){
 global.blackListCheck = function(itemToTest, event, player) {
     if (itemToTest) {
         var id = itemToTest.id.toString();
-        var itemBlackList = ['tconstruct:sword', 'tconstruct:cleaver', 'tconstruct:scythe', 'artifacts:everlasting_beef', 'artifacts:eternal_steak', 'artifacts:scarf_of_invisibility', 'immersiveengineering:windmill', 'immersiveengineering:watermill'];
+        var itemBlackList = ['tconstruct:sword', 'tconstruct:cleaver', 'tconstruct:scythe', 'artifacts:everlasting_beef', 'artifacts:eternal_steak', 'artifacts:scarf_of_invisibility', 'immersiveengineering:windmill', 'immersiveengineering:watermill', 'stalwart_dungeons:tungsten_helmet', 'stalwart_dungeons:tungsten_chestplate', 'stalwart_dungeons:tungsten_leggings', 'stalwart_dungeons:tungsten_boots', 'stalwart_dungeons:warted_tungsten_helmet', 'stalwart_dungeons:warted_tungsten_chestplate', 'stalwart_dungeons:warted_tungsten_leggings', 'stalwart_dungeons:warted_tungsten_boots', 'stalwart_dungeons:chorundum_armour_helmet', 'stalwart_dungeons:chorundum_armour_chestplate', 'stalwart_dungeons:chorundum_armour_leggings', 'stalwart_dungeons:chorundum_armour_boots'];
         if (relaxedContains(id, itemBlackList)) {
-            player.tell("You have Obtained an banned item, its recipe is either disabled or will be removed in the future.")
+            player.tell("You have Obtained an removed item, it either causes a crash or is considered OP")
             player.tell("Removed: "+id)
             itemToTest.count = 0;
             event.cancel();
