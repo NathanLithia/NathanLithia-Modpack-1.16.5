9b16c06c4cddceb9c71d7e522d5d10df4b7d567b
Nathan - Lithia <nathanlol5@live.com.au>
2022-03-30 03:03:18 +1030

    Worldedit

--- kubejs/startup_scripts/worldedit.js
+++ kubejs/startup_scripts/worldedit.js
@@ -3,4 +3,5 @@ onEvent('item.registry', event => {
   // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
   // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
   event.create('worldedit_wand').displayName('Worldedit Wand')
+  event.create('worldedit_compass').displayName('Worldedit Compass')
 })
