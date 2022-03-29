c7e726cbec3797276ff78c9545fcec11181d46bf
Nathan - Lithia <nathanlol5@live.com.au>
2022-03-30 04:53:39 +1030

    Update blacklist.js

--- kubejs/server_scripts/blacklist.js
+++ kubejs/server_scripts/blacklist.js
@@ -7,7 +7,7 @@ function relaxedContains (thing,array){
 global.blackListCheck = function(itemToTest, event, player) {
     if (itemToTest) {
         var id = itemToTest.id.toString();
-        var itemBlackList = ['tconstruct:sword', 'tconstruct:cleaver', 'tconstruct:scythe', 'artifacts:everlasting_beef', 'artifacts:eternal_steak', 'artifacts:scarf_of_invisibility', 'immersiveengineering:windmill', 'immersiveengineering:watermill', 'stalwart_dungeons:tungsten_helmet', 'stalwart_dungeons:tungsten_chestplate', 'stalwart_dungeons:tungsten_leggings', 'stalwart_dungeons:tungsten_boots', 'stalwart_dungeons:warted_tungsten_helmet', 'stalwart_dungeons:warted_tungsten_chestplate', 'stalwart_dungeons:warted_tungsten_leggings', 'stalwart_dungeons:warted_tungsten_boots', 'stalwart_dungeons:chorundum_armour_helmet', 'stalwart_dungeons:chorundum_armour_chestplate', 'stalwart_dungeons:chorundum_armour_leggings', 'stalwart_dungeons:chorundum_armour_boots'];
+        var itemBlackList = [];
         if (relaxedContains(id, itemBlackList)) {
             player.tell("You have Obtained an removed item, it either causes a crash or is considered OP")
             player.tell("Removed: "+id)
